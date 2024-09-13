241741668b2f8832cc7a6c5a9f07234d5108ca58
IvonneBell <agonzale4_22@alu.uabcs.mx>
2024-09-13 13:33:22 -0700

    Creacion de componentes

---
+++ src/components/CardHeader.jsx
@@ -0,0 +1,10 @@
+function CardHeader (params){
+    return (
+        <>
+        <div className="pokemon-name">Oshawott</div>
+        <div className="pokemon-id">#501</div>
+        </>
+    )
+}
+
+export default CardHeader
